c9ceac09761b03e690224763128f4455c5d8c045
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-05-19 20:40:22 +0600

    bid requests page almost complete

--- src/pages/BidRequests.jsx
+++ src/pages/BidRequests.jsx
@@ -1,10 +1,33 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { AuthContext } from "../provider/AuthProvider";
+
 const BidRequests = () => {
+  const { user } = useContext(AuthContext);
+  const [bids, setBids] = useState([]);
+
+  useEffect(() => {
+    getMyBids();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  const getMyBids = async () => {
+    try {
+      const { data } = await axios(`${import.meta.env.VITE_API_URL}/bid-requests/${user.email}`);
+      setBids(data);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  console.log(bids);
   return (
     <section className="container px-4 mx-auto pt-12">
       <div className="flex items-center gap-x-3">
         <h2 className="text-lg font-medium text-gray-800 ">Bid Requests</h2>
 
-        <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full ">05 Requests</span>
+        <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full ">{bids.length} Requests</span>
       </div>
 
       <div className="flex flex-col mt-6">
@@ -47,63 +70,74 @@ const BidRequests = () => {
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200 ">
-                  <tr>
-                    <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">Build Dynamic Website</td>
-                    <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">example@gmail.com</td>
+                  {bids.map((bid) => (
+                    <tr key={bid._id}>
+                      <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">{bid.job_title}</td>
+                      <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">{bid.email}</td>
 
-                    <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">10/04/2024</td>
+                      <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">{new Date(bid.deadline).toDateString()}</td>
 
-                    <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">$200</td>
-                    <td className="px-4 py-4 text-sm whitespace-nowrap">
-                      <div className="flex items-center gap-x-2">
-                        <p
-                          className="px-3 py-1 rounded-full text-blue-500 bg-blue-100/60
-                             text-xs"
+                      <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">${bid.price}</td>
+                      <td className="px-4 py-4 text-sm whitespace-nowrap">
+                        <div className="flex items-center gap-x-2">
+                          <p
+                            className={`px-3 py-1 rounded-full ${bid.category === "Web Development" && "text-blue-500 bg-blue-100/60"} ${
+                              bid.category === "Graphics Design" && "text-red-500 bg-red-100/60"
+                            } ${bid.category === "Digital Marketing" && "text-yellow-500 bg-yellow-100/60"}
+                               text-xs`}
+                          >
+                            {bid.category}
+                          </p>
+                        </div>
+                      </td>
+                      <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                        <div
+                          className={`inline-flex items-center px-3 py-1 rounded-full gap-x-2 ${
+                            bid.status === "Pending" && "bg-yellow-100/60 text-yellow-500"
+                          } 
+                          ${bid.status === "In Progress" && "bg-blue-100/60 text-blue-500"}
+                          ${bid.status === "Completed" && "bg-green-100/60 text-green-500"}
+                          `}
                         >
-                          Web Development
-                        </p>
-                      </div>
-                    </td>
-                    <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                      <div className="inline-flex items-center px-3 py-1 rounded-full gap-x-2 bg-yellow-100/60 text-yellow-500">
-                        <span className="h-1.5 w-1.5 rounded-full bg-yellow-500"></span>
-                        <h2 className="text-sm font-normal ">Pending</h2>
-                      </div>
-                    </td>
-                    <td className="px-4 py-4 text-sm whitespace-nowrap">
-                      <div className="flex items-center gap-x-6">
-                        <button className="text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            className="w-5 h-5"
-                          >
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-                          </svg>
-                        </button>
+                          <span className="h-1.5 w-1.5 rounded-full bg-yellow-500"></span>
+                          <h2 className="text-sm font-normal">{bid.status}</h2>
+                        </div>
+                      </td>
+                      <td className="px-4 py-4 text-sm whitespace-nowrap">
+                        <div className="flex items-center gap-x-6">
+                          <button className="text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="w-5 h-5"
+                            >
+                              <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                            </svg>
+                          </button>
 
-                        <button className="text-gray-500 transition-colors duration-200   hover:text-yellow-500 focus:outline-none">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            className="w-5 h-5"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636"
-                            />
-                          </svg>
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
+                          <button className="text-gray-500 transition-colors duration-200   hover:text-yellow-500 focus:outline-none">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="w-5 h-5"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
